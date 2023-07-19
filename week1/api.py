607e14d51968f1746e1ef30ae292f44bce6907b5
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-07-19 14:58:25 +0900

    week1

---
+++ week1/api.py
@@ -0,0 +1,57 @@
+import requests
+import xmltodict
+import json
+import logging
+
+# logger
+logger = logging.getLogger(name='MyLog')
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('|%(asctime)s||%(name)s||%(levelname)s|\n%(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S'
+                            )
+
+stream_handler = logging.StreamHandler() 
+stream_handler.setFormatter(formatter) 
+logger.addHandler(stream_handler) 
+
+# API URL
+url = "https://business.juso.go.kr/addrlink/addrLinkApi.do"
+
+# Parameters
+confmKey = "devU01TX0FVVEgyMDIzMDcxODE0MDUxNDExMzkzODk="
+currentPage = 1
+countPerPage = 10
+keyword = "서대문구 연세로 50"
+
+# Construct the URL with encoded parameters
+params = {
+    "confmKey": confmKey,
+    "currentPage": currentPage,
+    "countPerPage": countPerPage,
+    "keyword": keyword
+}
+encoded_params = "&".join([f"{key}={value}" for key, value in params.items()])
+full_url = f"{url}?{encoded_params}"
+
+# Send the GET request
+response = requests.get(full_url)
+
+# Process the response
+if response.status_code == 200:
+    xml_data = response.text
+    data_dict = xmltodict.parse(xml_data)
+    json_data = json.dumps(data_dict, ensure_ascii=False)
+    parsed_data = json.loads(json_data)
+    #logger.info(json_data)
+    #logger.info(parsed_data)
+    
+    # Process the data as needed
+else:
+    # Request failed
+    print(f"Request failed with status code {response.status_code}")
+
+for i in range(10):
+    road_addr = parsed_data['results']['juso'][i]['roadAddr']
+    logger.info(road_addr)
+    
---
+++ week1/week1.py
@@ -0,0 +1,118 @@
+import argparse
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+import logging
+import os
+import csv
+
+
+""" 
+1. ArgumentParser 설정 
+"""
+# ArgumentParser 객체 생성
+parser = argparse.ArgumentParser(description='이 프로그램의 설명')
+
+# 인자 추가하기
+parser.add_argument('--text', type=str, required=True, default= None, help='input text 값')
+parser.add_argument('--env', required=False, default='dev', help='실행환경은 뭐냐')
+
+conf = parser.parse_args()
+
+if conf.text is None:
+    print("\n No Text is supplied. Please input text. \n")
+elif conf.text.endswith(".txt"):
+    with open(conf.text, 'r', encoding='utf-8') as f:
+        text = f.read()
+        
+
+""" 
+2. Logger 설정
+"""
+# logger 객체 생성
+logger = logging.getLogger(name='MyLog')
+logger.setLevel(logging.INFO) ## 경고 수준 설정
+
+# log 의 format 
+formatter = logging.Formatter('|%(asctime)s||%(name)s||%(levelname)s|\n%(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S'
+                            )
+
+stream_handler = logging.StreamHandler() ## 스트림 핸들러 생성
+stream_handler.setFormatter(formatter) ## 텍스트 포맷 설정
+logger.addHandler(stream_handler) ## 핸들러 등록
+
+# log file 에 로그들을 모두 저장하고 싶을 때
+logging.basicConfig(filename='myinfo.log',level=logging.INFO)
+
+
+# https://huggingface.co/arpanghoshal/EmoRoBERTa 참고 
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
+
+emotion = pipeline('sentiment-analysis', 
+                    model='arpanghoshal/EmoRoBERTa')
+
+emotion_labels = emotion(text)
+#print(emotion_labels)
+logger.info(emotion_labels)
+
+
+""" 
+Q1. debug 수준은 출력될까 ? 
+""" 
+debug_test = "hi I am debug"
+logger.debug(debug_test)
+
+""" 
+Task1. 만약 file 내에 있는 모든 textfile 에 대해 결과를 csv 로 출력하고 싶으면 ? 
+"""
+
+# Define input folder and output file paths
+input_folder = 'textfile'
+output_file = 'emotion_results.csv'
+
+# Define CSV headers
+headers = ['file_name', 'label','score']
+
+
+# Initialize an empty list to store the results
+results = []
+
+# Iterate over the files in the input folder
+for filename in os.listdir(input_folder):
+    file_path = os.path.join(input_folder, filename)
+    
+    # Check if the file is a text file
+    if filename.endswith(".txt"):
+        # Read the text file
+        with open(file_path, 'r', encoding='utf-8') as f:
+            text = f.read()
+
+        # Process the text and get the results
+        rst_data_list = emotion(text)
+
+        # Iterate over the elements in the rst_data_list
+        for rst_data in rst_data_list:
+            # Create a dictionary for each result
+            result_dict = {
+                'file_name': os.path.splitext(filename)[0],
+                'label': rst_data['label'],
+                'score': rst_data['score']
+            }
+
+            # Append the result dictionary to the list
+            results.append(result_dict)
+        
+        logger.info(result_dict)
+
+# Write the results to the CSV file
+with open(output_file, 'w', newline='',encoding='utf-8-sig') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=headers)
+
+    # Write headers
+    writer.writeheader()
+
+    # Write rows
+    for result in results:
+        writer.writerow(result)
+
+print('Results saved to', output_file)
